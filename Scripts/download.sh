146fb97b6b4f2ad597b7f5083cbaae8fc8d43bb6
Diego Silva <diegosiuniube@gmail.com>
2016-11-03 14:19:13 -0200

    alteracoes

--- Scripts/download.sh
+++ Scripts/download.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -ev
 
-sdk_version=$(cat Scripts/SDK_VER)
-sha1_checksum=$(cat Scripts/SDK_SHA)
+sdk_version=$(cat SDK_VER)
+sha1_checksum=$(cat SDK_SHA)
 
 echo "fetching ActorSDK.framework"
-curl -OL https://github.com/actorapp/ActorSDK-iOS/releases/download/v${sdk_version}/ActorSDK.zip
+curl -OL https://github.com/dfsilva/ActorSDK-iOS/releases/download/v${sdk_version}/ActorSDK.zip
 
-echo "${sha1_checksum}  ActorSDK.zip" | shasum -c
+#echo "${sha1_checksum}  ActorSDK.zip" | shasum -c
 
 unzip -o -q ActorSDK.zip
 rm ActorSDK.zip
